ad4541998709df76c7b9b93251baf762f7629cfb
Kate Bonner <katebonner277@gmail.com>
2022-07-14 11:43:26 -0400

    initial set up

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      {
+        database: process.env.DB_NAME, 
+        username: process.env.DB_USER, 
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+module.exports = sequelize;
