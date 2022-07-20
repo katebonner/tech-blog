89b2d4b838b71fca4d2e4988bb9cf408723b6bd5
Kate Bonner <katebonner277@gmail.com>
2022-07-20 02:28:47 +0200

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,10 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(
-//       {
-//         database: process.env.DB_NAME, 
-//         username: process.env.DB_USER, 
-//         host: 'localhost',
-//         dialect: 'mysql'
-//     });
-
-const sequelize = new Sequelize(
+//create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
       {
         database: process.env.DB_NAME, 
         username: process.env.DB_USER, 
